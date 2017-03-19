6229c8cea9ec81477cbbc02e45a619e56daf9667
Anderson Fernandes <fernandesanderson14@gmail.com>
2017-03-19 15:23:46 -0300

    Adicionando bean validation para discente e servidor
    - ajustes nos formulários
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by anderson on 19/03/17.
+ */
+
+$( document ).ready(function() {
+    $(".cpf").mask("###.###.###-##");
+
+    $("#table").DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "zeroRecords": "Nada encontrado",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "Sem registros disponíveis",
+            "infoFiltered": "(filtrado de um _MAX_ total de registros)",
+            "search": "Procurar:",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            }
+        }
+    });
+});
