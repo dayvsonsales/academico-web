82149fbc8b9e9dad5035fb34a38ee3ec8a915a54
Anderson Fernandes <fernandesanderson14@gmail.com>
2017-03-22 19:52:26 -0300

    Adicionado postgres como banco de dados
    - adicionadas configurações dinâmicas de acesso ao banco
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/main/java/util/JpaUtil.java
+++ src/main/java/util/JpaUtil.java
@@ -3,6 +3,8 @@ package util;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by anderson on 11/03/17.
@@ -12,7 +14,20 @@ public class JpaUtil {
     private static EntityManagerFactory factory;
 
     static {
-        factory = Persistence.createEntityManagerFactory("AcademicoPU");
+        Map<String, String> env = System.getenv();
+        Map<String, Object> configOverrides = new HashMap<String, Object>();
+
+        for (String envName : env.keySet()) {
+            if (envName.contains("DATABASE_USER")) {
+                configOverrides.put("javax.persistence.jdbc.user", env.get(envName));
+            }else if (envName.contains("DATABASE_URL")) {
+                configOverrides.put("javax.persistence.jdbc.url", env.get(envName));
+            } else if (envName.contains("DATABASE_PASSWORD")) {
+                configOverrides.put("javax.persistence.jdbc.password", env.get(envName));
+            }
+        }
+
+        factory = Persistence.createEntityManagerFactory("AcademicoPU", configOverrides);
     }
 
     public static EntityManager getEntityManager(){
