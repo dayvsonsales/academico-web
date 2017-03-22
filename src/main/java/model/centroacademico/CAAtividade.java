4d810a4799c41c444663609a6f79b7686ff263ff
Anderson Fernandes <fernandesanderson14@gmail.com>
2017-03-22 18:56:10 -0300

    Corrigido problema com import
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/main/java/model/centroacademico/CAAtividade.java
+++ src/main/java/model/centroacademico/CAAtividade.java
@@ -1,12 +1,12 @@
 package model.centroacademico;
 
-import com.sun.istack.internal.NotNull;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
