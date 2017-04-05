40158685973c970ee787570d3df296f516ae1bbd
dayvsonsales <dayvson@outlook.com>
2017-04-04 23:50:46 -0300

    Contato Centro Academico

---
+++ src/main/java/repository/ContatoRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+import model.centroacademico.Contato;
+
+/**
+ * Created by Dayvson on 11/03/2017.
+ */
+public class ContatoRepository extends RepositoryBase<Contato> {
+    public ContatoRepository() {
+        super(Contato.class);
+    }
+}
